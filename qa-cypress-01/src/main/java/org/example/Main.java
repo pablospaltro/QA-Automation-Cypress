b1863b0724bd52c8fb3492e96a561a7ccdd34ebb
pablospaltro5@gmail.com <pablospaltro5@gmail.com>
2024-01-22 22:12:26 -0300

    Actualizo carpeta  de proyectos Cypress

---
+++ qa-cypress-01/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+
+        First short test with Cypress (cypress.e2e.spec.cy.js)
+
+        1. Visit: https://example.cypress.io
+        2. Find the element with content: type
+        3. Click on it
+        4. Get the URL
+        5. Assert it includes: /commands/actions
+        6. Get the input with the action-email data-testid
+        7. Type fake@email.com into the input
+        8. Assert the input reflects the new value
+
+        Given a user visits https://example.cypress.io
+        When they click the link labeled type
+        And they type "fake@email.com" into the [data-testid="action-email"] input
+        Then the URL should include /commands/actions
+        And the [data-testid="action-email"] input has "fake@email.com" as its value
+
+        */
+    }
+}
